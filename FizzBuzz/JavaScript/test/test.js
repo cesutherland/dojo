9dcb2038a7df1f90f140ca616cbc050684b4e5a9
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2013-02-09 14:29:25 -0300

    normalized indentations

--- FizzBuzz/JavaScript/test/test.js
+++ FizzBuzz/JavaScript/test/test.js
@@ -10,16 +10,16 @@ describe("FizzBuzz", function(){
 		var number = 1;
 		number.toString().should.equal("1");
 	});
-    it("should return Fizz for powers of three", function(){
-    	var number = 3;
-    	number.toString().should.equal("Fizz");
-    });
-    it("should return Buzz for powers of five", function(){
-    	var number = 5;
-    	number.toString().should.equal("Buzz");
-    });
-    it("should return FizzBuzz for powers of three and five", function(){
-    	var number = 15;
-    	number.toString().should.equal("FizzBuzz");
-    });
+		it("should return Fizz for powers of three", function(){
+			var number = 3;
+			number.toString().should.equal("Fizz");
+		});
+		it("should return Buzz for powers of five", function(){
+			var number = 5;
+			number.toString().should.equal("Buzz");
+		});
+		it("should return FizzBuzz for powers of three and five", function(){
+			var number = 15;
+			number.toString().should.equal("FizzBuzz");
+		});
 })
